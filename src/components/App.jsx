af6ab4026755bcc1308b38da155dbb8b2093358d
Chariey-01 <charityjepkoech007@gmail.com>
2026-05-11 03:35:01 +0300

    This is Toylab submission

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,29 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
+  const [toys, setToys] = useState([]);
   const [showForm, setShowForm] = useState(false);
 
-  function handleClick() {
-    setShowForm((showForm) => !showForm);
-  }
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, []);
 
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
-      <div className="buttonContainer">
-        <button onClick={handleClick}>Add a Toy</button>
-      </div>
-      <ToyContainer />
+
+      {showForm && <ToyForm setToys={setToys} />}
+
+      <button onClick={() => setShowForm(!showForm)}>
+        Add a Toy
+      </button>
+
+      <ToyContainer toys={toys} setToys={setToys} />
     </>
   );
 }
--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, setToys }) {
+  function handleLike() {
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ likes: toy.likes + 1 }),
+    })
+      .then((res) => res.json())
+      .then((updatedToy) => {
+        setToys((prev) =>
+          prev.map((t) => (t.id === updatedToy.id ? updatedToy : t))
+        );
+      });
+  }
+
+   function handleDelete() {
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: "DELETE",
+    }).then(() => {
+      setToys((prev) => prev.filter((t) => t.id !== toy.id));
+    });
+  }
+
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{toy.name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={toy.image /* Toy's Image URL */}
+        alt={toy.name /* Toy's Name */}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{toy.likes} Likes </p>
+      <button className="like-btn" onClick={handleLike}>
+        Like {"<3"}
+      </button>
+      <button className="del-btn" onClick={handleDelete}>
+        Donate to GoodWill
+      </button>
     </div>
   );
 }
